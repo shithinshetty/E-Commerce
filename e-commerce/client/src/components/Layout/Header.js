f62ff7bd091ee636ebda079381b3c3a2d3ff7731
shithinshetty <shithinshetty07@gmail.com>
2023-10-06 00:32:13 +0530

    Added Logout

--- e-commerce/client/src/components/Layout/Header.js
+++ e-commerce/client/src/components/Layout/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { TiShoppingCart } from "react-icons/ti";
+
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
 const Header = () => {
+  const [auth, setAuth] = useAuth();
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user: null,
+      token: "",
+    });
+    localStorage.removeItem("auth");
+    toast.success("Logout Sucess");
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -32,16 +44,32 @@ const Header = () => {
                   Category
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink to="/register" className="nav-link">
-                  Register
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/login" className="nav-link">
-                  Login
-                </NavLink>
-              </li>
+              {auth.user ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink
+                      onClick={handleLogout}
+                      to="/login"
+                      className="nav-link"
+                    >
+                      Logout
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink to="/register" className="nav-link">
+                      Register
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/login" className="nav-link">
+                      Login
+                    </NavLink>
+                  </li>{" "}
+                </>
+              )}
               <li className="nav-item">
                 <NavLink to="/cart" className="nav-link">
                   Cart(0)
--- e-commerce/client/src/pages/HomePAge.js
+++ e-commerce/client/src/pages/HomePAge.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
 
 const HomePAge = () => {
+  const [auth, setAuth] = useAuth();
   return (
     <Layout title={" E-commerce21"}>
       <h1>Home Page</h1>
+      <pre> {JSON.stringify(auth, null, 4)}</pre>
     </Layout>
   );
 };
